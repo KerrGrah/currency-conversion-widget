3d8cdb62ab433bcb89256cd2ee0512036bc92f4c
Graham <grey.ham.acre@gmail.com>
2018-02-25 19:13:01 +0300

    add svg arrows

--- src/components/CentralSwitch.js
+++ src/components/CentralSwitch.js
@@ -1,15 +1,54 @@
 import React, { Component } from "react";
 import styled from "styled-components";
+import svg from "../assets/switch";
 
-export default props => <Container onClick={props.switchSelected} />;
+export default class Swicth extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      clicked: false
+    };
+  }
+
+  handleClick = switchSelected => {
+    this.setState(
+      ({ clicked }) => ({ clicked: !clicked }),
+      // let animation finish before action dispatch
+      () => {
+        setTimeout(() => {
+          switchSelected();
+        }, 350);
+      }
+    );
+  };
+  render() {
+    return (
+      <Container
+        clicked={this.state.clicked}
+        onClick={this.handleClick.bind(null, this.props.switchSelected)}
+      >
+        {svg("#666")}
+      </Container>
+    );
+  }
+}
 
 const Container = styled.div`
   position: absolute;
   margin-left: 10%;
   top: 185px;
-  width: 40px;
+  width: 30px;
   height: 30px;
+  padding: 6px;
   border-radius: 20px;
   background: #fff;
+  cursor: pointer;
   z-index: 1;
+  transition: transform 400ms ease-in-out;
+  ${({ clicked }) => (clicked ? "transform: rotate(180deg)" : "")};
+  @media (max-width: 490px) {
+    top: 180px;
+    width: 40px;
+    height: 40px;
+  }
 `;
